359326eacab8fa686ac532df8d2d074b931565f5
chadonyi <chigbo.c.o@gmail.com>
2020-03-12 12:56:08 +0100

    first commit

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const app = express()
 
 app.get('/', function(req, res){
     res.send('Home page')
-
 })
 
 let port = process.env.PORT || 3000
 
 app.listen(port, function(){
-    console.log('app is runing at port 3000 ########')
+    console.log('app is runing at port 3000....')
 })
 
 
