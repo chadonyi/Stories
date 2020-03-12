849dabddd5634b236d07ea7e4018b9e65d515dfc
chadonyi <chigbo.c.o@gmail.com>
2020-03-12 12:27:43 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let express = require('express')
+let mongoose = require('mongoose')
+
+const app = express()
+
+app.get('/', function(req, res){
+    res.send('Home page')
+
+})
+
+let port = process.env.PORT || 3000
+
+app.listen(port, function(){
+    console.log('app is runing at port 3000 ########')
+})
+
+
+
